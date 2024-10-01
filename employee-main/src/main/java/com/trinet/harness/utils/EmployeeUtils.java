1803fa49db64c1267fc6b359c6bf501176d1b6f8
Sridhar Rao Eravelli <71243647+Sridharraoe@users.noreply.github.com>
2024-10-01 17:03:36 +0530

    Update EmployeeUtils.java

--- employee-main/src/main/java/com/trinet/harness/utils/EmployeeUtils.java
+++ employee-main/src/main/java/com/trinet/harness/utils/EmployeeUtils.java
@@ -15,12 +15,12 @@ public class EmployeeUtils {
 	}
 	public static List<Employee> employeeList = new ArrayList<>();
 	static {
-		employeeList.add(new Employee(1, "Ramesh", LocalDate.of(1990, 2, 1), "HR", 12000));
-		employeeList.add(new Employee(2, "Rajesh", LocalDate.of(1991, 5, 14), "HR", 15000));
-		employeeList.add(new Employee(3, "Sahil", LocalDate.of(1993, 2, 23), "SALES", 12000));
-		employeeList.add(new Employee(4, "Shekhar", LocalDate.of(1990, 2, 24), "SALES", 15000));
-		employeeList.add(new Employee(5, "James", LocalDate.of(1990, 2, 15), "IT", 12000));
-		employeeList.add(new Employee(6, "Rosy", LocalDate.of(1993, 2, 15), "IT", 15000));
+		employeeList.add(new Employee(1, "GET /employees/{employeeId}", LocalDate.of(1990, 2, 1), "emp-microservice", 12000));
+		employeeList.add(new Employee(2, "PUT /employees/{employeeId}", LocalDate.of(1991, 5, 14), "emp-microservice", 15000));
+		employeeList.add(new Employee(3, "DELETE /employees/{employeeId}", LocalDate.of(1993, 2, 23), "emp-microservice", 12000));
+		employeeList.add(new Employee(4, "GET /employees/", LocalDate.of(1990, 2, 24), "emp-microservice", 15000));
+		//employeeList.add(new Employee(5, "James", LocalDate.of(1990, 2, 15), "IT", 12000));
+		//employeeList.add(new Employee(6, "Rosy", LocalDate.of(1993, 2, 15), "IT", 15000));
 	}
 
 	public static List<Employee> getEmployeesList(Set<String> dept) {
